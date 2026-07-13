2068ad19793ed7d64e042f54144ac4a24525fdab
buxtonabraham65 <97378587+buxtonabraham65@users.noreply.github.com>
2026-07-13 14:39:47 +0000

    Update ALLOWED_HOSTS for production settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'z+ksf@)0d^qojbh4rnp4b1to$hq&*tt(3bs$gf(3i267g$k9ln'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['duxtcars-new-update-2026.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
