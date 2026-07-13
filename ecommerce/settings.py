a71a78ea357875306e41ab0b87e7ea580f67c5af
buxtonabraham65 <97378587+buxtonabraham65@users.noreply.github.com>
2026-07-13 16:12:50 +0000

    Configure database settings for production

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,10 +84,11 @@ import os
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
+
 
 
 
